fcd6b9ad865eeeeb9cc8b815132c61fd6aa5a376
Sai D <daverpallysaiarun@gmail.com>
2022-09-04 17:27:47 -0400

    Updated Date-Time Display and Copy-To-Clipboard Icon

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import moment from 'moment'
 import eventHandler from '../eventHandler'
 import { useNavigate } from 'react-router-dom'
-import { TbFileSearch, TbEdit, TbCopy } from 'react-icons/tb'
+import { TbFileSearch, TbEdit } from 'react-icons/tb'
 import { IoTrashOutline } from 'react-icons/io5'
+import { HiOutlineClipboardCopy } from 'react-icons/hi'
 
 const Dashboard = () => {
 	const allNotes = eventHandler.GET()
@@ -14,46 +15,60 @@ const Dashboard = () => {
 			{allNotes.length === 0 && (
 				<h3 className="not-found-msg">No Notes Found!</h3>
 			)}
-			{allNotes.map((note) => (
-				<div className="note-container" key={note._id}>
-					<h2 style={{ marginBottom: 0 }}>
-						Note #{allNotes.indexOf(note) + 1}
-					</h2>
-					<p>{moment(note.createdAt).format('LL - hh:mm A')}</p>
-					<div className="icon-container">
-						<TbCopy
-							title="Copy Note Link..."
-							className="copy-icon"
-							onClick={() => {
-								const link = `/view-note/${note._id}`
-								navigator.clipboard
-									.writeText(link)
-									.then(() => alert('Note Link Copied...'))
-							}}
-						/>
-						<TbFileSearch
-							title="View This Note..."
-							className="search-icon"
-							onClick={() => navigate(`/view-note/${note._id}`)}
-						/>
-						<TbEdit
-							title="Edit This Note..."
-							className="edit-icon"
-							onClick={() =>
-								navigate(`/edit-note/${note._id}`, {
-									replace: true,
-									state: { body: note.body },
-								})
-							}
-						/>
-						<IoTrashOutline
-							title="Delete This Note..."
-							className="delete-icon"
-							onClick={() => eventHandler.DELETE(note._id)}
-						/>
+			{allNotes.map((note) => {
+				const createdAt = moment(note.createdAt).format('L - hh:mm A')
+				const updatedAt = moment(note.updatedAt).format('L - hh:mm A')
+				return (
+					<div className="note-container" key={note._id}>
+						<h2 style={{ marginBottom: 0 }}>
+							Note #{allNotes.indexOf(note) + 1}
+						</h2>
+						{createdAt === updatedAt ? (
+							<p className='date-time'>
+								<em>Created At: </em>
+								{createdAt}
+							</p>
+						) : (
+							<p className='date-time'>
+								<em>Updated At: </em>
+								{updatedAt}
+							</p>
+						)}
+						<div className="icon-container">
+							<HiOutlineClipboardCopy
+								title="Copy This Note Link..."
+								className="copy-icon"
+								onClick={() => {
+									const link = `/view-note/${note._id}`
+									navigator.clipboard
+										.writeText(link)
+										.then(() => alert('LINK COPIED SUCCESSFULLY...'))
+								}}
+							/>
+							<TbFileSearch
+								title="View This Note..."
+								className="search-icon"
+								onClick={() => navigate(`/view-note/${note._id}`)}
+							/>
+							<TbEdit
+								title="Edit This Note..."
+								className="edit-icon"
+								onClick={() =>
+									navigate(`/edit-note/${note._id}`, {
+										replace: true,
+										state: { body: note.body },
+									})
+								}
+							/>
+							<IoTrashOutline
+								title="Delete This Note..."
+								className="delete-icon"
+								onClick={() => eventHandler.DELETE(note._id)}
+							/>
+						</div>
 					</div>
-				</div>
-			))}
+				)
+			})}
 		</div>
 	)
 }
